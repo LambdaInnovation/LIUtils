35c15a007036e97ac9bc734f506c6c536254942a
WeAthFolD <cheetah77@qq.com>
2015-08-28 17:19:55 +0800

    nothing BLockFilter

--- src/main/java/cn/liutils/util/mc/BlockFilters.java
+++ src/main/java/cn/liutils/util/mc/BlockFilters.java
@@ -6,9 +6,6 @@ import net.minecraft.world.World;
 
 public class BlockFilters {
 	
-	/**
-	 * This filter filters NO blocks, so it collides with water or grass blocks.
-	 */
 	public static final IBlockFilter filNothing = new IBlockFilter() {
 
 		@Override
@@ -25,6 +22,15 @@ public class BlockFilters {
 			Block b = world.getBlock(x, y, z);
 			return b.getCollisionBoundingBoxFromPool(world, x, y, z) != null && 
 					b.canCollideCheck(world.getBlockMetadata(x, y , z), false);
+		}
+		
+	},
+	
+	filEverything = new IBlockFilter() {
+		
+		@Override
+		public boolean accepts(World world, int x, int y, int z, Block block) {
+			return false;
 		}
 		
 	};
